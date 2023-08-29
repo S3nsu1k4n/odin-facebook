22bf8bbc4e2d40cea5ffcbf32b4c1817838425e8
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-29 11:59:04 +0900

    show list of users on users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,17 @@ class UsersController < ApplicationController
 
   def show
     # profile information, photo and posts
-    @user = current_user
+    @user = param_user_id ? User.find(param_user_id) : current_user
   end
 
   def index
     # list all users and buttons for sending friend requests
     @users = User.all
+  end
+
+  private
+
+  def param_user_id
+    params.permit(:id)[:id]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.delete_all
+
+User.create(name: "test", email: "test@email.com", password: "testtest")
+User.create(name: "test2", email: "test2@email.com", password: "testtest")
+User.create(name: "test3", email: "test3@email.com", password: "testtest")
+User.create(name: "test4", email: "test4@email.com", password: "testtest")
+User.create(name: "test5", email: "test5@email.com", password: "testtest")
+User.create(name: "test6", email: "test6@email.com", password: "testtest")
+
+p "Users created: #{User.all.count}"
