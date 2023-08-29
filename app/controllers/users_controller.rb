f102e5872dfd0a8f6e74ef59d6e324991b08f12e
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-29 11:06:24 +0900

    add user#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    # profile information, photo and posts
+    @user = current_user
+  end
+
+  def index
+    # list all users and buttons for sending friend requests
+    @users = User.all
+  end
 end
