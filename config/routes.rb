9cfbcebb64030dc6b6f00d0c52c085e66ae6381a
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-29 17:56:47 +0900

    add post#new view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
+  resources :posts, only: [:new, :create]
   # Defines the root path route ("/")
   root "users#show"
 end
