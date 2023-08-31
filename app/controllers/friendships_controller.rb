2d99883c06a79a2538fd07184b637284cc3fa277
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-31 11:48:54 +0900

    add sending friend request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,20 @@
 class FriendshipsController < ApplicationController
+  def create
+    @user = User.find(friendship_params[:friend_id])
+
+    if current_user.friends.include?(@user) || current_user == @user
+      flash.now[:notify] = "Already friends with #{@user.name}"
+      @users = User.all
+    else
+      Friendship.create(user_id: current_user.id, friend_id: friendship_params[:friend_id])
+      @users = User.all
+    end
+    render 'users/index'
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:friend_id)
+  end
 end
