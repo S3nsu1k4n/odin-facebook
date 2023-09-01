250e6974b7e87dd018b35c8e683a0d52f4544df9
Sascha Koeppe <saschakoeppe@gmail.com>
2023-09-01 14:51:01 +0900

    add comments model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_021704) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_054406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
